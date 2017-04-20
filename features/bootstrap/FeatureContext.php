b4667c60c304d93b8ef3914a106eb7cd90b160cb
Ilya Pleshakov <da-eto@ya.ru>
2017-04-20 11:24:53 +0300

    [wip] add "I need to see products" on feature + add symfony context to behat config for future access the kernel

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -4,12 +4,15 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use Behat\Symfony2Extension\Context\KernelDictionary;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext implements Context
 {
+    use KernelDictionary;
+
     /**
      * Initializes context.
      *
@@ -19,5 +22,13 @@ class FeatureContext extends MinkContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given /^I have product "([^"]*)" on shop$/
+     */
+    public function iHaveProductOnShop($product)
+    {
+        throw new \Behat\Behat\Tester\Exception\PendingException();
     }
 }
--- features/mainpage.feature
+++ features/mainpage.feature
@@ -1,8 +1,9 @@
 Feature: Main page
   In order to use Any Shop
   As a website user
-  I need to be able to open main page
+  I need to be able to open main page and see some products
 
   Scenario: Open Any Shop main page
-    Given I am on "/"
+    Given I have product "ThinkPad X1 Yoga" on shop
+    And I am on "/"
     Then I should see "Any Shop"
